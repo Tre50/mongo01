f5ba5cf12d131b56309bad071cbc5b3ddc908d01
Tremayne Johnson <tremayne4@gmail.com>
2023-10-12 16:07:46 -0400

    fish

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { addAFish, getAllFish } from "./src/fish.js";
+import { addAFish, getAllFish,deleteAFish, updateOneFish } from "./src/fish.js";
 import { closeClient } from "./src/connectDb.js";
 
 await addAFish()
 await getAllFish()
+await deleteAFish()
+await updateOneFish()
 
 closeClient();
--- src/fish.js
+++ src/fish.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { db } from "./connectDb.js";
 
 
@@ -6,14 +7,49 @@ const theFish = {
     origin: 'Amazon',
     color: "silver",
     size: '1"',
-
+    family: { dad: '🐠',
+        sister:'🐡',
+        mom: { cool: true, special: undefined },
+        
+},
 };
 // add a fish
 export async function addAFish() {
-    await db.collection('fish').insertOne(theFish)
+    const fishAdded = await db.collection('fish').insertOne(theFish)
+    console.log('fishAdded ->', fishAdded)
+
 
 }
+//get all fish
 export async function getAllFish() {
     const fishList = await db.collection('fish').find({}).toArray();
     console.table(fishList);
 }
+
+
+
+export async function deleteAFish() {
+    const fishDelete = await db.collection('fish').findOneAndDelete({species: 'tetra'})
+
+const myCleanId = ObjectId('652821360693d034a1999ca1')
+
+    // delete by id
+    const fishDelete = db.collection('fish').findOneAndDelete({_id:myCleanId})
+
+   console.log('fishDelete -> ', fishDelete)
+    //find the fish i want delete
+} 
+
+// update Fish
+export async function updateOneFish(){
+    
+    const updateOneFish = await fishdb.findOneAndUpdate({_id: myCleanId }, { $set: updateOneFish })
+    console.log('updateOneFish ->', updateOneFish)
+
+    //define the fish
+    //define what to update
+    //udpate which fish
+    //then update
+}
+    
+
